c8d6e02beccc366d42d39b18cbb567ef3207b579
Jon Duke <jduke99@gmail.com>
2018-07-25 23:58:45 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -26,11 +26,7 @@
         var docs = smart.patient.api.fetchAll({
             type: 'DocumentReference',
           query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
+                    
                 }
           });
 
